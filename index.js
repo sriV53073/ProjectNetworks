408bbf186075e8f003dbfd67f39f9b7f0f342042
Sri Vemugunta <svemugunta@gmail.com>
2022-04-26 12:01:03 -0500

    added the log

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-
+const fd =  require('fs');
+const fs = require('fs').promises;
 const port = process.env.PORT || 1337;
-
 server.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
@@ -13,6 +12,18 @@ io.on('connection', (socket) => {
     console.log('a user connected');
    // console.log(socket.id);
     socket.on('join', function (data) {
+      try { // Create files directory if not exists
+        if (!fd.existsSync(data.name+".txt"))
+        {
+         fs.appendFile(data.name+".txt", data.name + " is logged in\n", 'utf8');
+        }
+        else
+        {
+          fs.appendFile(data.name+".txt", data.name + " is logged in\n", 'utf8');
+        }
+      } catch (e) {
+        console.log.error("error:" + e)
+      }
         socket.join(data.name);
         // you check if the name was created before, if it was you acess the text file, if not you create new text file for that name
       });
@@ -20,6 +31,10 @@ io.on('connection', (socket) => {
       // socket.on for close
       // delete this 
       socket.on('hello', function (data) {
+
+ (async () => {
+    await fs.appendFile(data.name+".txt", data.val + "\n", 'utf8');
+  })();
         try{
         var num = eval(data.val);
         io.sockets.in(data.name).emit('helloRep', {val:num });
@@ -31,6 +46,9 @@ io.on('connection', (socket) => {
       });
       socket.on('exit', function (data){
         console.log(data.name + " has exited");
+        (async () => {
+          await fs.appendFile(data.name+".txt", data.name + " has exited\n", 'utf8');
+        })();
       });
     //socket.on('funk')
     // you get a arifunc in data, you tokenize that function and run and send the ans back to client, you have to log on that specific client name text file
