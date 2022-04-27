4f944f8ac75216dad6e493499d2f12e22d10d8ca
Sri Vemugunta <svemugunta@gmail.com>
2022-04-26 22:41:10 -0500

    Comments

--- client.js
+++ client.js
@@ -1,43 +1,40 @@
+//Socket information used to connect to Index.js
 const io = require('socket.io-client');
 var readlineSync = require('readline-sync');
 const socket = io.connect('http://localhost:1337/');
+
+//See if the link between client and index was made and get a equation
 socket.on('connect', () => {
   console.log('Successfully connected!\n');
   console.log("Enter the function you want to be sent over")
-  // delete the function below
+  
+  //Print the result of the equation and then repeat until terminated
   socket.on('helloRep', function (data) {
-    console.log("Value Of Function is " + data.val)// We are using room of socket io
+    console.log("Value Of Function is " + data.val)
     console.log("Enter the function you want to be sent over")
   });
-  // add a function where you socket.on('answer') console.log(data.answer)
 });
+
+//Request the name of the user, and print it back
 var name1 = readlineSync.question('May I have your name? ');
   console.log(`Hi ${name1}!`)
   n1 = name1;
+
+  //try to connect to Index.js
   socket.emit('join', {name: name1});
   var stdin = process.openStdin();
+
   stdin.addListener("data", function(d) {
-      // note:  d is an object, and when converted to a string it will
-      // end with a linefeed.  so we (rather crudely) account for that  
-      // with toString() and then trim() 
+      //see if an exit statment is sent, if exit was sent terminate the process 
       if(d.toString().toLowerCase().trim() === "exit")
       {
         socket.emit('exit', {name: n1});
         console.log("You have exited\n");
         process.exit(1);
       }
+      //If there was no exit sent, send the string as the name of the user
       else
       {
         socket.emit('hello', {val: d.toString().trim(), name:n1});
       }
     });
-
-
-// While( true)
-// get the user input for the artithimetic function
-//socket.emit('func',{val:arithfunc});
-//if input is exit
-//socket.emit'exit'
-//exit while and close
-//Exit while
-//delete
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//the imports
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -5,58 +6,72 @@ const io = require('socket.io')(server);
 const fd =  require('fs');
 const fs = require('fs').promises;
 const port = process.env.PORT || 1337;
+
+//Listen to the port for clients
 server.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
+
+//When connected to a client
 io.on('connection', (socket) => {
+  //Get the starting time, and log the user as connected
+  var timetracker = Date.now();
     console.log('a user connected');
-   // console.log(socket.id);
+
+    //Create a file, or append an existing file when the client joins
     socket.on('join', function (data) {
-      try { // Create files directory if not exists
+      try { 
+        // Create files directory if not exists
         if (!fd.existsSync(data.name+".txt"))
         {
          fs.appendFile(data.name+".txt", data.name + " is logged in\n", 'utf8');
         }
+        //If the file exists it will append to the end
         else
         {
           fs.appendFile(data.name+".txt", data.name + " is logged in\n", 'utf8');
         }
-      } catch (e) {
+      }
+      //If it fails to create a file report error
+      catch (e) {
         console.log.error("error:" + e)
       }
-        socket.join(data.name);
-        // you check if the name was created before, if it was you acess the text file, if not you create new text file for that name
-      });
-      //socket.on math thats arithemitic and send to that specific client the answer
-      // socket.on for close
-      // delete this 
-      socket.on('hello', function (data) {
 
- (async () => {
-    await fs.appendFile(data.name+".txt", data.val + "\n", 'utf8');
-  })();
+      //creates a private room between the client and the server
+        socket.join(data.name);
+      });
+
+      //When it's working get the name
+      socket.on('hello', function (data) {
+        (async () => {
+          await fs.appendFile(data.name+".txt", data.val + "\n", 'utf8');
+        })();
+        //Makes sure the eval function correctly works, if it does return the value to helloRep
         try{
         var num = eval(data.val);
         io.sockets.in(data.name).emit('helloRep', {val:num });
         }
+        //if eval doen't work report error
         catch{
         io.sockets.in(data.name).emit('helloRep', {val:"Error"});
         }
-        
       });
+
+      //when exit is typed in terminal
       socket.on('exit', function (data){
-        console.log(data.name + " has exited");
+        //get the end time and put the exit time in the logs
+        timetracker = Date.now() - timetracker;
+        console.log(data.name + " has exited after a session lasting "+timetracker+" milliseconds.");
+        
+        //doesn't allow multiple clients to edit one file at the same time
         (async () => {
-          await fs.appendFile(data.name+".txt", data.name + " has exited\n", 'utf8');
+          await fs.appendFile(data.name+".txt", data.name + " has exited after a session lasting "+timetracker+" milliseconds.\n", 'utf8');
         })();
       });
-    //socket.on('funk')
-    // you get a arifunc in data, you tokenize that function and run and send the ans back to client, you have to log on that specific client name text file
-    // exit, search up how to force close client connection in socket.io
-    //socket.on('exit')
+    
+    //On disconnect
     socket.on('disconnect', () => {
       //console.log('user disconnected');
     });
   });
-  // text file, you just log the arthimetic function into a text file, with the name and function asked. One folder and 
-  //we should find a way to track time server side
+  
